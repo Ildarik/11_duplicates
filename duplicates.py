875415b95463b319a71cd87157dc42d45c5002b5
Nogmanov Ildar <nogmanov@gmail.com>
2017-08-23 09:12:41 +0300

    Get defaultdict with unique key (name, size)

--- duplicates.py
+++ duplicates.py
@@ -1,13 +1,17 @@
 import os
+from collections import defaultdict
 from sys import argv
 
+
+def get_files_dict(path_dir):
+    result_dict = defaultdict(list)
+    for root, dirs, files in os.walk(path_dir):
+        for file_name in files:
+            full_file_path = os.path.join(root, file_name)
+            file_size = os.path.getsize(full_file_path)
+            result_dict[(file_name, file_size)].append(full_file_path)
+    return result_dict
+
+
 if __name__ == '__main__':
-    path = argv[1]
-    all_files = []
-
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            all_files.append([file, "/".join((root, file))])
-
-    for item in sorted(all_files):
-        print(item)
+    path_dir = argv[1]
